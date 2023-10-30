e3953c8bb6ddf3b87c70324989d361b691540717
yihong1120 <qazsx748596@gmail.com>
2023-10-31 01:54:27 +0800

    Add Dockerfile

--- chatbo_web/chatbot_manager/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class ChatLog(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    log_file = models.FileField(upload_to='logs/')
-    json_file = models.FileField(upload_to='chat_records/')
