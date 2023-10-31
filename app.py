61b7626e46c0dfc8b7cb3faa67c680408ad24571
YiHungWONG <40423264+yihong1120@users.noreply.github.com>
2023-10-31 12:53:47 +0800

    Add type hint, docstring, comments

--- app.py
+++ app.py
@@ -9,9 +9,17 @@ from utils import config_manager, chat_manager, model_manager
 base_context = config_manager.load_base_context()
 
 async def start(update: Update, context: CallbackContext) -> None:
+    """
+    Start the chatbot for a user.
+
+    Args:
+        update (Update): The update received from Telegram.
+        context (CallbackContext): The context associated with this update.
+    """
     user_id = update.message.from_user.id
     
     try:
+        # Ensure the chat file for this user exists
         chat_manager.ensure_chat_file_exists(user_id)
         logging.info(f"User {user_id} started the bot.")
         await update.message.reply_text('Welcome to the Llama Chatbot! Type your message.')
@@ -19,10 +27,18 @@ async def start(update: Update, context: CallbackContext) -> None:
         logging.error(f"Error starting chat for user {user_id}: {e}")
 
 async def chat(update: Update, context: CallbackContext) -> None:
+    """
+    Process a chat message from a user.
+
+    Args:
+        update (Update): The update received from Telegram.
+        context (CallbackContext): The context associated with this update.
+    """
     user_input = update.message.text
     user_id = update.message.from_user.id
 
     try:
+        # Load the conversation history for this user
         convo = chat_manager.load_chat(user_id)
         
         # Add the latest message to the conversation
@@ -41,6 +57,12 @@ async def chat(update: Update, context: CallbackContext) -> None:
         logging.error(f"Error processing chat for user {user_id} with input '{user_input}': {e}")
 
 def main() -> None:
+    """
+    The main function to start the chatbot.
+
+    This function sets up logging, initializes the bot with a token, 
+    adds a message handler for text messages, and starts the bot.
+    """
     # Set up logging
     config_manager.setup_logging()
 
@@ -49,6 +71,7 @@ def main() -> None:
     bot = Bot(token=TOKEN)
     app = Application.builder().token(TOKEN).build()
 
+    # Add a message handler for text messages (excluding commands)
     text_filter = (filters.TEXT & ~filters.COMMAND)
     app.add_handler(MessageHandler(text_filter, chat))
 
